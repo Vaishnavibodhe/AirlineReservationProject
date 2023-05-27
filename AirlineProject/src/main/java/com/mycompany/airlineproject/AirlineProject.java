4d07a189cef938afa289529e6e6edff8ff4252f1
Vaishnavibodhe <128290200+Vaishnavibodhe@users.noreply.github.com>
2023-05-27 22:45:53 +0530

    secondcommit

---
+++ AirlineProject/src/main/java/com/mycompany/airlineproject/AirlineProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.airlineproject;
+
+/**
+ *
+ * @author vaishnavi
+ */
+public class AirlineProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
